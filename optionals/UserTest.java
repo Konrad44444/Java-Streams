79b7f26526de1be86de4a7d7b6acb670e0ee7b60
Konrad Warzecha <konrad_warzecha@wp.pl>
2023-04-20 18:57:29 +0200

    Accessing the Value of Optional Objects

--- optionals/UserTest.java
+++ optionals/UserTest.java
@@ -1,5 +1,7 @@
 package optionals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.NoSuchElementException;
@@ -8,6 +10,8 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;   
     
 public class UserTest {
+    final String NAME = "Bulbek";
+    // --- Creating Optionals ---
 
     @Test
     public void whenCreateEmptyOptional_thenNull() throws Exception{
@@ -32,5 +36,37 @@ public class UserTest {
             Optional<User> emptyUser = Optional.ofNullable(user);
         });
     }
+
+    // --- Accessing Optinals' value ---
+
+    @Test
+    public void whenCreateOfNullableOptional_thenOk() throws Exception {
+        String name = NAME;
+
+        //will throw an exception if the value is null
+        Optional<String> nameOpt = Optional.ofNullable(name);
+        assertEquals(NAME, nameOpt.get());
+    }
+
+    //checking whether the value in optional is not null
+    @Test
+    public void whenCheckIfPresent_thenOk() throws Exception {
+        User u = new User(NAME, "123");
+        Optional<User> userOpt = Optional.ofNullable(u);
+
+        assertTrue(userOpt.isPresent());
+        assertEquals(NAME, userOpt.get().getName());
+    }
+
+    //using ifPresent() method
+    @Test
+    public void ifPresentMethodTest() throws Exception {
+        User u = new User(NAME, "123");
+        Optional<User> userOpt = Optional.ofNullable(u);
+
+        userOpt.ifPresent(user -> assertEquals(NAME, user.getName()));
+    }
+    
+
 }
     
