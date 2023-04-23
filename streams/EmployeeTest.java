82082db66b0c50fb964d38bdc6a0bc343707c6e3
Konrad Warzecha <konrad_warzecha@wp.pl>
2023-04-23 15:32:07 +0200

    Stream Operations

--- streams/EmployeeTest.java
+++ streams/EmployeeTest.java
@@ -1,7 +1,13 @@
 package streams;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -24,12 +30,13 @@ public class EmployeeTest {
         new Employee(ID3, NAME3, SALARY3)
     };
 
-    static List<Employee> employeesList = Arrays.asList(employeesArray);
-
+    
     // - stream from array
     Stream<Employee> streamFromArray = Stream.of(employeesArray);
     
     // - stream from list
+    static List<Employee> employeesList = Arrays.asList(employeesArray);
+    
     Stream<Employee> streamFromList = employeesList.stream();
 
     // - stream from individual objects
@@ -44,10 +51,108 @@ public class EmployeeTest {
         .add(employeesArray[2])
         .build();
     
+    // --- Stream Operations ---
 
+    // - forEach()
     @Test
-    public void test() {
-        
+    public void streamForEachTest() throws Exception {
+        streamFromList.forEach(e -> e.salaryIncrement(10.0f));
+
+        assertAll(
+            () -> assertEquals(employeesList.get(0).getSalary(), 1.1 * SALARY1),
+            () -> assertEquals(employeesList.get(1).getSalary(), 1.1 * SALARY2),
+            () -> assertEquals(employeesList.get(2).getSalary(), 1.1 * SALARY3)
+        );
+
+        // forEach() is a terminal operation, which means that,
+        // after the operation is performed, the stream pipeline is considered consumed, and can no longer be used
     }
+
+    // - map()
+    // produces a new stream after applying a function to each element of the original stream
+    // the new stream could be of different type
+    @Test
+    public void mapTest() throws Exception {
+        List<String> employeesNames = streamFromArray
+            .map(Employee::getName)
+            .collect(Collectors.toList());
+
+        assertEquals(employeesArray.length, employeesNames.size());
+    }
+
+    // - collect()
+    //  get stuff out of the stream once all the processing is done
+    @Test
+    public void collectTest() throws Exception {
+        List<Employee> testEmployeesList = employeesList.stream().collect(Collectors.toList());
+        
+        assertEquals(testEmployeesList, employeesList);
+    }
+
+    // - filter()
+    // produces a new stream that contains elements of the original stream that pass a given test
+    @Test
+    public void filterTest() throws Exception {
+        List<Employee> result = streamFromArray
+            .filter(i -> i.getId() == 2)
+            .collect(Collectors.toList());
+
+        assertEquals(result.size(), 1);
+    }
+
+    // - findFirst()
+    // returns an Optional for the first entry in the stream
+    @Test
+    public void findFirstTest() throws Exception {
+        Optional<Employee> employeeOptional = streamFromArray
+            .filter(e -> e.getSalary() > SALARY1)
+            .findFirst();
+
+        // two employees has bigger salary than SALARY1 but the first found is in optional
+        assertEquals(employeeOptional.orElse(null).getSalary(), SALARY2);
+    }
+
+    // - toArray()
+    @Test
+    public void toArrayTest() throws Exception {
+        Employee[] testEmployeesArray = streamFromArray.toArray(Employee[]::new);
+
+        assertEquals(testEmployeesArray.length, employeesArray.length);
+    }
+
+    // - flatMap()
+    // stream can hold complex data structures like Stream<List<String>>
+    // flatMap() helps to flatten the data structure to simplify further operations
+    @Test
+    public void flatMap() throws Exception {
+        List<List<String>> namesNested = Arrays.asList( 
+            Arrays.asList("Jeff", "Bezos"), 
+            Arrays.asList("Bill", "Gates"), 
+            Arrays.asList("Mark", "Zuckerberg")
+        );
+
+        List<String> names = namesNested.stream()
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+
+        assertEquals(names.size(), namesNested.size() * 2);
+    }
+
+    // - peek()
+    // peek does the same things as forEach() but stream can be used more times
+    @Test
+    public void testPeek() throws Exception {
+        List<Employee> testEmployeeList =  employeesList.stream()
+            .peek(e -> e.salaryIncrement(10.0f))
+            .peek(System.out::println)
+            .collect(Collectors.toList());
+
+        assertAll(
+            () -> assertEquals(testEmployeeList.get(0).getSalary(), SALARY1 * 1.1),
+            () -> assertEquals(testEmployeeList.get(1).getSalary(), SALARY2 * 1.1),
+            () -> assertEquals(testEmployeeList.get(2).getSalary(), SALARY3 * 1.1)
+        );
+    }
+
 }
     
